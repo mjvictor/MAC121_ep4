26142036ad2249a9fc71468a9a6155f654c688ce
mjvictor <victor_mjoao@hotmail.com>
2016-10-27 01:02:01 -0200

    nothing very effective

--- tabelaSimbolo_VO.h
+++ tabelaSimbolo_VO.h
@@ -12,25 +12,8 @@ typedef struct {
 	int max;
 }SymbolTable;
 
-SymbolTable *create_stable () {
-	SymbolTable *table;
-	table = malloc (sizeof (SymbolTable));
-	table->v = malloc (1024 * sizeof (pair));
-	table->max = 1024;
-	table->i = 0;
-	for (i = 0; i < 1024; i++) {
-		table->v[i].key = malloc (100 * sizeof (char));
-		table->v[i].freq = 0;
-	}
-	return table;
-}
+SymbolTable *create_stable ();
 
-void put_stable (SymbolTable *table, char *key) {
-	
-
-	if (table->i == table->max) {
-
-	}
-}
+void put_stable (SymbolTable *table, char *key);
 
 #endif
