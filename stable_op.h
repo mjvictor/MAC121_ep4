9b18dd44a8fcb3b49307f2681a3b002c9a35fda1
mjvictor <victor_mjoao@hotmail.com>
2016-11-02 00:01:18 -0200

    Add the header of stable operations

---
+++ stable_op.h
@@ -0,0 +1,26 @@
+#ifndef _STABLE_OP_H_
+#define _STABLE_OP_H_
+
+#include <stdio.h>
+typedef struct {
+	char *key;
+	int freq;
+}pair;
+
+typedef struct {
+	pair *v;
+	int i;
+	int max;
+}SymbolTable;
+
+SymbolTable *create_stable ();
+
+void reallocate_stable (SymbolTable *table);
+
+void put_word (SymbolTable *table, int i, char *key);
+
+void destroy_stable (SymbolTable *table);
+
+void print_stable (SymbolTable *table);
+
+#endif
