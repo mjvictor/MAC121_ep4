57f91f4e21e7be85899f2745cb4f705d0f69ff09
mjvictor <victor_mjoao@hotmail.com>
2016-11-01 23:58:32 -0200

    partial .c

--- tabelaSimbolo.c
+++ tabelaSimbolo.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-
-
+#include "buffer.h"
+#include "stable_op.h"
+#include "tabelaSimbolo_VD.h"
 
 int main (int argc, char **argv) {
-	int struct_type, ordenation_type;
+	/*char *struct_type, */
+	char *ordenation_type;
+	
 	FILE *input;
 
 	input = fopen (argv[1], "r");
 
-	struct_type = argv[2];
+/*
+	Buffer *word;
+	struct_type = strdup (argv[2]);
 
-	ordenation_type = argv[3];
-
+	ordenation_type = strdup (argv[3]);
+	
 	switch (struct_type) {
 		case "VD":
-
+	
 			break;
 		case "VO":
 
@@ -31,5 +37,21 @@ int main (int argc, char **argv) {
 			break;
 	}
 	
+	word = buffer_create();
+
+	while (read_word(input, word) != -1) {
+		print_buffer (word);
+	} 
+
+	print_buffer (word);
+	buffer_destroy (word);
+	*/
+
+	ordenation_type = argv[2];
+
+	printf("%s\n", ordenation_type);
+
+	unsorted_vector (input, argv[2]);
+
 	return 0;
 }
