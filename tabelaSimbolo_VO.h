9a58456e00799e708978d0be3fa7afd5bed63ffb
mjvictor <victor_mjoao@hotmail.com>
2016-11-03 22:26:55 -0200

    add a lot of funcitions, but don't work

--- tabelaSimbolo_VO.h
+++ tabelaSimbolo_VO.h
@@ -1,19 +1,16 @@
 #ifndef _TABELASIMBOLO_VO_H_
 #define _TABELASIMBOLO_VO_H_
+#include "stable_op.h"
 
-typedef struct {
-	char *key;
-	int freq;
-}pair;
 
-typedef struct {
-	pair *v;
-	int i;
-	int max;
-}SymbolTable;
+int find_wordBin (SymbolTable *table, char *key);
 
-SymbolTable *create_stable ();
+int find_freq (SymbolTable *table, int x);
 
-void put_stable (SymbolTable *table, char *key);
+void insert_VO (SymbolTable *table, int i, char *key);
+
+void correct_freq (SymbolTable *table, int i, int j);
+
+void sorted_array (FILE *input, char *type);
 
 #endif
