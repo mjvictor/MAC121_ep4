3859087ed5a176176ba6ef2f27590201d95f963f
mjvictor <victor_mjoao@hotmail.com>
2016-11-07 00:16:17 -0200

    add sorted linked list

---
+++ tabelaSimbolo_LO.h
@@ -0,0 +1,9 @@
+#ifndef _TABELASIMBOLO_LO_H_
+#define _TABELASIMBOLO_LO_H_
+#include "linked_op.h"
+
+pointer insert_sorted (pointer start, char *key);
+
+void sorted_linked (FILE *input, char *ordenation_type);
+
+#endif
