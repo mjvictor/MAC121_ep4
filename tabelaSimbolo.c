8d45123ee3a93a96d7413b3a6a0c0ddd45debe1b
mjvictor <victor_mjoao@hotmail.com>
2016-11-07 00:20:45 -0200

    add switch function

--- tabelaSimbolo.c
+++ tabelaSimbolo.c
@@ -1,57 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "buffer.h"
-#include "stable_op.h"
 #include "tabelaSimbolo_VD.h"
+#include "tabelaSimbolo_VO.h"
+
+void switch_type (FILE *input, char *struct_type, char *sort_type) {
+	if (!(strcmp(sort_type, "VD")))
+		unsorted_array (input, struct_type);
+	else if (!(strcmp(sort_type, "VO")))
+		sorted_array (input, struct_type);
+	else if (!(strcmp(sort_type, "LD")))
+		unsorted_linked (input, struct_type);
+	else if (!(strcmp(sort_type, "LO")))
+		sorted_linked (input, struct_type);
+	else 
+		binary_tree (input, struct_type);
+}
 
 int main (int argc, char **argv) {
-	/*char *struct_type, */
-	char *ordenation_type;
-	
 	FILE *input;
 
 	input = fopen (argv[1], "r");
 
-/*
-	Buffer *word;
-	struct_type = strdup (argv[2]);
-
-	ordenation_type = strdup (argv[3]);
-	
-	switch (struct_type) {
-		case "VD":
-	
-			break;
-		case "VO":
-
-			break;
-		case "LO":
-
-			break;
-		case "LD":
-
-			break;
-		case "AB":
-		
-			break;
+	if (!(strcmp(argv[2], "VD"))) {
+		unsorted_vector (input, argv[3]);
 	}
-	
-	word = buffer_create();
-
-	while (read_word(input, word) != -1) {
-		print_buffer (word);
-	} 
-
-	print_buffer (word);
-	buffer_destroy (word);
-	*/
-
-	ordenation_type = argv[2];
-
-	printf("%s\n", ordenation_type);
-
-	unsorted_vector (input, argv[2]);
+	else {
+		sorted_array (input, argv[3]);
+	}
 
 	return 0;
 }
