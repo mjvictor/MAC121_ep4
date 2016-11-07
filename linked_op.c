3a87a75f08213238600ce39c289285459bd4769e
mjvictor <victor_mjoao@hotmail.com>
2016-11-07 00:11:44 -0200

    add operations for linked list

--- linked_op.c
+++ linked_op.c
@@ -1,20 +1,41 @@
 #include <stdlib.h>
+#include <string.h>
 #include "linked_op.h"
 
-pointer create_celula (int n) {
+pointer create_celula (char *key) {
 	pointer new;
 	new = malloc (sizeof(celula));
 	new->info = malloc(sizeof(pair));
-	new->info.key = malloc (n * sizeof(char));
-	new->info.freq = 0;
+	new->info.key = strdup (key);
+	new->info.freq = 1;
 	return new;
 }	
 
-void insert_start (apontador *start, pair v) {
+void insert_start (apontador *start, char *key) {
 	pointer p;
-	p = malloc (sizeof (celula));
-	p->info.key = strdup (v.key);
-	p->info.freq = v.freq;
+	p = create_celula (key);
 	p->prox = *start;
 	*start = p;
+}
+
+pointer find_linked (pointer start, char *key) {
+	if (start == NULL) return NULL;
+	if (!strcmp(start->info.key, key)) return start;
+	return (find_linked(start->prox, key));
+}
+
+void print_linked (pointer start) {
+	pointer p;
+	for (p = start; p != NULL; p = p->prox) 
+		printf ("%s %d\n", p->info.key, p->info.freq);
+}
+
+void destroy_linked (pointer start) {
+	pointer p, ant;
+	for (p = start, ant = NULL; p != NULL; ant = p, p = p->prox) {
+		free (p->info.key);
+		if (ant != NULL) {
+			free (ant);
+		}
+	}
 }
